1f9032cc6ed5d4f5bec13c3c078bbbb917ed9673
MedicationTime <loicreboursiere@gmail.com>
2023-06-20 16:38:30 +0200

    Addition of TFMini-S fzpz and of computations.h

--- src/computations.h
+++ src/computations.h
@@ -3,7 +3,8 @@
  * @file computations.h
  * @author Loïc Reboursière - UMONS, CLick (loic.reboursiere@umons.ac.be)
  * @brief This code is involved in the development of a 3D motorized LiDAR scanner based on servotomors. 
- * This file includes all the structures and function that compute the cloud points obtained from the scanner.
+ * This file includes all the structures and function that compute the cloud points and the various distances and measures of the object.
+ * This code, in the vast majority, needs to be tested in deep, as it has barely been integrated in the main code.
  * 3DLidarScanner-v1 – CLICK - UMONS (Loïc Reboursière) is free software: you can redistribute it and/or modify it under the terms of the Apache License Version 2.0. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Apache License Version 2.0 for more details.
  * You should have received a copy of the Apache License Version 2.0 along with this program.  If not, see http://www.apache.org/licenses/
  * Each use of this software must be attributed to University of MONS – CLICK (Loïc Reboursière).
@@ -24,6 +25,15 @@
 extern  const uint8_t rows;
 extern  const uint8_t cols;
 
+/** @struct Dimension
+ *  @brief This structure is used to store 3D dimensions of an object.
+ *  @var Dimension::height 
+ *  Height of the object.
+ *  @var Dimension::width 
+ *  Width of the object.
+ *  @var Dimension::depth 
+ *  Depth of the object.
+ */
 typedef struct Dimension
 {
     int height;
@@ -31,6 +41,14 @@ typedef struct Dimension
     int depth;
 } Dimension;
 
+/** @struct GroundsHeights
+ *  @brief This structure is used to store the different "grounds" of a specific space, 
+ * the actual ground of a support that can be added to the scene.
+ *  @var GroundsHeights::distanceToGround 
+ *  The distance from the sensor position to the actual ground.
+ *  @var GroundsHeights::distanceToSupport 
+ *  The distance from the sensor position to the support position.
+ */
 typedef struct GroundsHeights
 {
     float distanceToGround  = -1.;
@@ -77,6 +95,17 @@ void subtractCloudPoint( CloudPoint background[], CloudPoint withObject[], Cloud
     }
     
 }
+
+/**
+ * @fn float computeDistance ( Point3D start, Point3D stop ) 
+ *
+ * @brief Compute distance between two Point3D structures
+ * 
+ * @param start Cloud point of the background, without object
+ * @param stop Cloud point of the background with the object
+ * 
+ * @return distanceBtwStartStop The actual distance between the two points. 
+ */
 float computeDistance ( Point3D start, Point3D stop ) 
 {
     float distanceBtwStartStop = sqrt( pow( ( stop.x - start.x ), 2 ) 
@@ -93,16 +122,16 @@ float computeDistance ( Point3D start, Point3D stop )
 // Soit deux passes : la première pour le sol (le plus bas) la deuxième pour le support (le plsu bas après le sol)
 // Soit mémoire de qq échantillons et trouver les valeurs identiques et dont les index se suivent
 
-GroundsHeights groundAndSupportDetectionWindowing( Point3D cloud [] )
-{
-    float z_max = 0, z_min = DISTANCE_MAX;
-    int idx_z_max = -1;
-
-    GroundsHeights heights;
-
-    return heights;
-}
-
+/**
+ * @fn GroundsHeights groundAndSupportDetection2Passes( Point3D cloud [] )
+ * 
+ * @brief Compute the distances from the sensor to the ground and to the support object (e.g. table, box, etc.), if present, 
+ * from the background cloud point variable. The cloud point is represented by an array of 3DPoint structures.
+ * 
+ * @param cloud Cloud point of the background, without object
+ * 
+ * @return heights GroundsHeights structure containing both distances 
+ */
 GroundsHeights groundAndSupportDetection2Passes( Point3D cloud [] )
 {
 
@@ -135,6 +164,16 @@ GroundsHeights groundAndSupportDetection2Passes( Point3D cloud [] )
 
 }
 
+/**
+ * @fn GroundsHeights computeHeights( uint16_t distances [] )
+ * 
+ * @brief Compute the distances from the sensor to the ground and to the support object (e.g. table, box, etc.), if present, 
+ * from the background cloudpoint variable. The cloud point is represented by an array of unint16_t values.
+ * 
+ * @param distances Array of distances gathered from the scan of the background, without object.
+ * 
+ * @return heights GroundsHeights structure containing both distances. 
+ */
 GroundsHeights computeHeights( uint16_t distances [] )
 {
 
@@ -194,91 +233,16 @@ GroundsHeights computeHeights( uint16_t distances [] )
 
 
 
-/* 
-Point3D computeBaseHeight( uint16_t distances [] )
-{
-
-    float x = 0, y = 0, z = 0;
-    float z_min = DISTANCE_MAX, z_max = 0, x_min = DISTANCE_MAX, x_max = 0, y_min = DISTANCE_MAX, y_max = 0;
-    int idx_z_min = -1, idx_z_max = -1, idx_x_min = -1, idx_x_max = -1, idx_y_min = -1, idx_y_max = -1;
-    int height = -1;
-    uint8_t h_index = -1, v_index = -1, h_angle = -1, v_angle = -1;
-
-    Point3D distances3D;
-
-    Serial.println( "------ Start Base computation" );
-
-    for( int i = 0; i < ARRAY_NB_ELEMENTS ; i++ )
-    {
-        if ( distances[ i ] > 0 )
-        {
-            oneLineIndex2HorizAndVertIndex( i, &h_index, &v_index );
-            pointIndexes2PointAngles( h_index, v_index, &h_angle, &v_angle ); // Still useful ?
-            computeXYZ( &x, &y, &z, distances[ i ], h_angle, v_angle );
-
-            if( z < z_min )
-            {
-                z_min = z;
-                idx_z_min = i;
-            }
-            else if( z > z_max )
-            {
-                z_max = z;
-                idx_z_max = i;
-            }
-
-            if( x < x_min ) 
-            {
-                x_min = x;
-                idx_x_min = i;
-            }
-                
-            else if( x > x_max )
-            {
-                x_max = x;
-                idx_x_max = i;
-            }
-
-            if( y < y_min )
-            {
-                y_min = y;
-                idx_y_min = i;
-            }
-            else if( y > y_max )
-            {
-                y_max = y;
-                idx_y_max = i;
-            }
-
-        }
-    }
-
-    Serial.println( "------ Stop Base computation" );
-
-    distances3D.x = x_max - x_min;
-    distances3D.y = y_max - y_min;
-    distances3D.z = z_max - z_min;
-
-    Serial.println("x_min " + String( x_min ) + " (idx : " + String( idx_x_min ) + ")" );
-    Serial.println("x_max " + String( x_max ) + " (idx : " + String( idx_x_max ) + ")" );
-    Serial.println("y_min " + String( y_min ) + " (idx : " + String( idx_y_min ) + ")" );
-    Serial.println("y_max " + String( y_max ) + " (idx : " + String( idx_y_max ) + ")" );
-    Serial.println("z_min " + String( z_min ) + " (idx : " + String( idx_z_min ) + ")" );
-    Serial.println("z_max " + String( z_max ) + " (idx : " + String( idx_z_max ) + ")" );
-    
-
-    float distanceBtwStartStop = sqrt( pow( ( x_max - x_min ), 2 ) 
-                                      + pow( ( y_max - y_min ), 2 ) 
-                                      + pow( ( z_max - z_min ), 2 ) 
-                                      );
-
-    Serial.println( "--- distanceBtwStartStop " + String( distanceBtwStartStop ) );
-
-
-    return distances3D;
-} 
-*/
-
+/**
+ * @fn int computeObjectHeight( uint16_t distances [] )
+ * 
+ * @brief Compute the height on the object based on the array of distances representing the cloud point of the object 
+ * (obtained after the subtraction of the two scanned cloud points).
+ * 
+ * @param distances Array of distances representing the object, result of the subtraction of the two scans.
+ * 
+ * @return height Height of the object. 
+ */
 int computeObjectHeight( uint16_t distances [] )
 {
     // Get min and max on the z axis of the 3D corrdinates 
@@ -340,48 +304,18 @@ int computeObjectHeight( uint16_t distances [] )
 
     return height;
 }
-/*
-int computeObjectHeight( CloudPoint cloudObject [] )
-{
-    // Get min and max on the z axis of the 3D corrdinates 
-    float z_min = 0, z_max = 0;
-    int idx_z_min = -1, idx_z_max = -1;
-    int height = -1;
 
-    // find the two CloudPoints where z is minimum and z is max
-    printf( "computeObjectHeight -- Starting Height computation\n" );
-    for( int i = 0; i < sizeof(cloudObject) / sizeof(CloudPoint) ; i++ )
-    {
-        if( cloudObject[ i ].coord3D.z < z_min )
-        {
-            z_min = cloudObject[ i ].coord3D.z;
-            idx_z_min = i;
-        }
-        else if( cloudObject[ i ].coord3D.z > z_max )
-        {
-            z_max = cloudObject[ i ].coord3D.z;
-            idx_z_max = i;
-        }
-    }
-
-    height = cloudObject[idx_z_max].scanCoord.distance - cloudObject[idx_z_min].scanCoord.distance;
-    printf( "computeObjectHeight -- Computed Height %i\n", height );
-    
-    return height;
-}
-*/
-/*
-void subtractMatrices( const int& ligne, const int& columns, int [][columns] bg, int [][rocows] img, int [][rows] result )
-{
-    for( int i = 0; i < rows; i++ )
-    {
-        for( int j = 0; j < columns; j++ )
-        {
-            result[ i ][ j ] = img[ i ][ j ] - bg[ i ][ j ];
-        }
-    }
-}
-*/
+/**
+ * @fn void subtractMatrices( int * bg, int * img, int * result )
+ * 
+ * @brief Subtract the two cloud points obtained after each scan.
+ *
+ * @param[in]  bg A pointer to the background cloudpoint.
+ * @param[in]  img A point to the scene with object cloudpoint.
+ * @param[out] result The result of the subtraction of the two cloudpoint.
+ *
+ * @return Nothing
+ */
 void subtractMatrices( int * bg, int * img, int * result )
 {
     for( int i = 0; i < rows; i++ )
@@ -393,57 +327,5 @@ void subtractMatrices( int * bg, int * img, int * result )
     }
 }
 
-void printScanPointCloud( CloudPoint cloud [] )
-{
-    for( int i = 0; i < ( sizeof( cloud ) / sizeof( cloud[ 0 ] ) ); i++ )
-    {
-        //int cols_idx = i / cols;
-        //int rows_idxnb_rows
-    } 
-}
-
-void printMatrix(int *arr, const uint8_t m, const uint8_t n)
-{
-    uint8_t i, j;
-    for (i = 0; i < m; i++) {
-      for (j = 0; j < n; j++)
-      {
-        Serial.print( *( ( arr + i * n ) + j ) );
-        Serial.print( ' ' );
-        //printf("%d ", *((arr+i*n) + j));
-      }
-      Serial.println();
-    }
-}
-/*
-void printMatrix( int ** mat, int rows, int columns )
-{
-    for( int i = 0; i < rows; i++ )
-    {
-        for( int j = 0; j < columns; j++ )
-        {
-            Serial.print( mat[ i ][ j ] );
-            Serial.print( ' ' );
-        }
-        Serial.println();
-    }
-}
-*/
-//int detectObject ( int A[][], int B [][], int * height, int * width, int ) {}
-
-  // We can also use "print(&arr[0][0], m, n);"
- /*
-  Serial.println( "###### Matrix dimensions" );
-  Serial.print( rows );
-  Serial.print( " : " );
-  Serial.println( cols );
-  Serial.println( "###### bg" );
-  printMatrix( (int *)bg, rows, cols);
-  Serial.println( "###### currentImg" );
-  printMatrix( (int *)currentImg, rows, cols);
-  subtractMatrices( (int *)bg, (int *)currentImg, (int *)result );
-  Serial.println( "###### result" );
-  printMatrix( (int *)result, rows, cols);
-  */
 
 #endif
